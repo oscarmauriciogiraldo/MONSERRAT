d15f1647b500dd71c75bcff6d1cef3a841d721ac
oscarmauriciogiraldo <oscardan@utp.edu.co>
2023-03-08 02:24:41 -0500

    ultima coleccion

---
+++ src/app/pages/landing-monserrat/categorias/model/colecciones.model.ts
@@ -0,0 +1,19 @@
+export interface Lienas{
+  title: string;
+  image: string;
+  //atributo opcional
+  description?: string;
+  products?: Products[];
+}
+
+export interface Products{
+  name: string;
+  description?: string;
+  image?: string;
+}
+
+export interface lastColection{
+  name: string;
+  description?: string;
+  image?: string;
+}
